5f6118e19f9c943729dce54ff6c0c7b3a9f38b56
Manuel <30572287+manuel-rw@users.noreply.github.com>
2025-08-23 00:36:51 +0200

    feat: add confetti to register page

--- menu-frontend/src/main.ts
+++ menu-frontend/src/main.ts
@@ -3,10 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import Particles from '@tsparticles/vue3'
+import { loadFull } from 'tsparticles'
+
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App).use(Particles, {
+  init: async (engine) => {
+    await loadFull(engine)
+  },
+})
 
 app.use(createPinia())
 app.use(router)
--- menu-frontend/src/views/auth/Register.vue
+++ menu-frontend/src/views/auth/Register.vue
@@ -1,4 +1,146 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const particleConfig = {
+  fullScreen: {
+    zIndex: 1,
+  },
+  emitters: [
+    {
+      position: {
+        x: 0,
+        y: 30,
+      },
+      rate: {
+        quantity: 5,
+        delay: 0.15,
+      },
+      life: {
+        count: 1,
+        duration: 1.5
+      },
+      particles: {
+        move: {
+          direction: 'top-right',
+          outModes: {
+            top: 'none',
+            left: 'none',
+            default: 'destroy',
+          },
+        },
+      },
+    },
+    {
+      position: {
+        x: 100,
+        y: 30,
+      },
+      rate: {
+        quantity: 5,
+        delay: 0.15,
+      },
+      life: {
+        count: 1,
+        duration: 1.5
+      },
+      particles: {
+        move: {
+          direction: 'top-left',
+          outModes: {
+            top: 'none',
+            right: 'none',
+            default: 'destroy',
+          },
+        },
+      },
+    },
+  ],
+  particles: {
+    color: {
+      value: ['#ffffff', '#FF0000'],
+    },
+    move: {
+      decay: 0.05,
+      direction: 'top',
+      enable: true,
+      gravity: {
+        enable: true,
+      },
+      outModes: {
+        top: 'none',
+        default: 'destroy',
+      },
+      speed: {
+        min: 10,
+        max: 50,
+      },
+    },
+    number: {
+      value: 0,
+    },
+    opacity: {
+      value: 1,
+    },
+    rotate: {
+      value: {
+        min: 0,
+        max: 360,
+      },
+      direction: 'random',
+      animation: {
+        enable: true,
+        speed: 30,
+      },
+    },
+    tilt: {
+      direction: 'random',
+      enable: true,
+      value: {
+        min: 0,
+        max: 360,
+      },
+      animation: {
+        enable: true,
+        speed: 30,
+      },
+    },
+    size: {
+      value: {
+        min: 4,
+        max: 7,
+      },
+      animation: {
+        enable: true,
+        startValue: 'min',
+        count: 1,
+        speed: 16,
+        sync: true,
+      },
+    },
+    roll: {
+      darken: {
+        enable: true,
+        value: 25,
+      },
+      enable: true,
+      speed: {
+        min: 5,
+        max: 15,
+      },
+    },
+    wobble: {
+      distance: 30,
+      enable: true,
+      speed: {
+        min: -7,
+        max: 7,
+      },
+    },
+    shape: {
+      type: ['circle', 'square'],
+      options: {},
+    },
+  },
+}
+</script>
 
 <template>
   <div class="block bg-white mt-10! p-5 py-24 mx-4 rounded-3xl">
@@ -57,6 +199,8 @@
       </button>
     </div>
   </div>
+
+  <vue-particles id="tsparticles" :options="particleConfig" />
 </template>
 
 <style scoped></style>
