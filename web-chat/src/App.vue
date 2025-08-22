451fcf84e22c6813eef7eedd2487b707016d8645
Manuel <30572287+manuel-rw@users.noreply.github.com>
2025-08-22 20:54:23 +0000

    feat: add web chat (#1)
    * feat: add web chat project
    
    * feat: add tailwind css
    
    * feat: add
    
    * feat: improve chat design
    
    * Dockerfile and nginx config created.
    
    * fix: wide screen centering
    
    ---------
    
    Co-authored-by: Christoph Keller <christoph.keller@dotnetcorner.ch>

---
+++ web-chat/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <nav class="bg-white px-6 py-4">
+    <RouterLink to="/" class="flex flex-row items-center gap-5">
+      <img alt="Vue logo" class="logo w-auto h-16" src="@/assets/transgourmet-short.png" />
+      <span class="text-3xl font-normal text-red-600 font-poetsen-one">Menu Mingle</span>
+    </RouterLink>
+  </nav>
+
+  <main id="main-sec" class="grow min-h-0">
+    <RouterView />
+  </main>
+</template>
+
+<style scoped></style>
---
+++ web-chat/src/views/ChatView.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+  import { IconUser, IconFridge, IconSend } from "@tabler/icons-vue";
+</script>
+
+<template>
+  <div class="p-10 h-full flex flex-col grow gap-4">
+    <div class="w-3/4">
+      <div class="flex flex-row gap-3">
+        <div class="bg-neutral-300 rounded-full w-10 h-10 p-2 aspect-square">
+          <IconFridge class="w-full h-full text-neutral-600" />
+        </div>
+        <div class="text-neutral-700 bg-neutral-300 p-4 pt-3 rounded-2xl">
+          <span class="block font-bold text-blue-500">Fridge</span>
+          <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="w-3/4">
+      <div class="flex flex-row gap-3">
+        <div class="bg-neutral-300 rounded-full w-10 h-10 p-2 aspect-square">
+          <IconUser class="w-full h-full text-neutral-600" />
+        </div>
+        <div class="text-neutral-700 bg-neutral-300 p-4 pt-3 rounded-2xl">
+          <span class="block font-bold text-green-600">Mom</span>
+          <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="w-3/4 self-end">
+      <div class="flex flex-row-reverse gap-3">
+        <div class="bg-neutral-300 rounded-full w-10 h-10 p-2 aspect-square">
+          <IconUser class="w-full h-full text-neutral-600" />
+        </div>
+        <div class="text-neutral-800 bg-red-200 p-4 pt-3 rounded-2xl">
+          <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="rounded-tl-2xl rounded-tr-2xl bg-red-200 mx-4! px-5 py-4 drop-shadow-[0_-10px_40px_rgba(0,0,0,0.15)]">
+    <div class="w-full flex flex-row gap-5">
+      <input type="text" class="bg-white rounded-2xl grow px-4 outline-4 outline-transparent focus:outline-rose-600" placeholder="Type a message..." />
+      <button class="bg-rose-600 hover:bg-rose-700 cursor-pointer rounded-3xl px-4 aspect-square text-white">
+        <IconSend />
+      </button>
+    </div>
+  </div>
+</template>
+
+<style>
+#main-sec {
+  display: flex;
+  flex-direction: column;
+}
+</style>
