e988475eae70edf61f54dea4e0474dc6ea122491
Manuel <30572287+manuel-rw@users.noreply.github.com>
2025-08-22 22:33:34 +0200

    feat: improve chat design

--- web-chat/src/App.vue
+++ web-chat/src/App.vue
@@ -4,14 +4,14 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <nav class="bg-white px-6 py-4 text-neutral-600">
+  <nav class="bg-white px-6 py-4">
     <RouterLink to="/" class="flex flex-row items-center gap-5">
-      <img alt="Vue logo" class="logo w-auto h-16" src="@/assets/transgourmet.svg" />
-      <span class="text-3xl font-normal font-poetsen-one">Menu Mingle</span>
+      <img alt="Vue logo" class="logo w-auto h-16" src="@/assets/transgourmet-short.png" />
+      <span class="text-3xl font-normal text-red-600 font-poetsen-one">Menu Mingle</span>
     </RouterLink>
   </nav>
 
-  <main id="main-sec" class="grow min-h-0">
+  <main id="main-sec" class="grow min-h-0 bg-gray-100">
     <RouterView />
   </main>
 </template>
--- web-chat/src/views/ChatView.vue
+++ web-chat/src/views/ChatView.vue
@@ -3,13 +3,13 @@
 </script>
 
 <template>
-  <div class="bg-gray-100 p-10 h-full flex flex-col grow gap-4">
+  <div class="p-10 h-full flex flex-col grow gap-4">
     <div class="w-3/4">
       <div class="flex flex-row gap-3">
         <div class="bg-neutral-300 rounded-full w-10 h-10 p-2 aspect-square">
           <IconFridge class="w-full h-full text-neutral-600" />
         </div>
-        <div class="text-neutral-800 bg-neutral-300 p-4 pt-3 rounded-2xl">
+        <div class="text-neutral-700 bg-neutral-300 p-4 pt-3 rounded-2xl">
           <span class="block font-bold text-blue-500">Fridge</span>
           <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</span>
         </div>
@@ -21,7 +21,7 @@
         <div class="bg-neutral-300 rounded-full w-10 h-10 p-2 aspect-square">
           <IconUser class="w-full h-full text-neutral-600" />
         </div>
-        <div class="text-neutral-800 bg-neutral-300 p-4 pt-3 rounded-2xl">
+        <div class="text-neutral-700 bg-neutral-300 p-4 pt-3 rounded-2xl">
           <span class="block font-bold text-green-600">Mom</span>
           <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</span>
         </div>
@@ -39,7 +39,7 @@
       </div>
     </div>
   </div>
-  <div class="rounded-tl-2xl rounded-tr-2xl bg-red-200 mx-4! px-5 py-4">
+  <div class="rounded-tl-2xl rounded-tr-2xl bg-red-200 mx-4! px-5 py-4 drop-shadow-[0_-10px_40px_rgba(0,0,0,0.15)]">
     <div class="w-full flex flex-row gap-5">
       <input type="text" class="bg-white rounded-2xl grow px-4 outline-4 outline-transparent focus:outline-rose-600" placeholder="Type a message..." />
       <button class="bg-rose-600 hover:bg-rose-700 cursor-pointer rounded-3xl px-4 aspect-square text-white">
