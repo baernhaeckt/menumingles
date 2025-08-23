44439a8de2830de80996e46224506e4cf0879adc
Manuel <30572287+manuel-rw@users.noreply.github.com>
2025-08-23 15:06:47 +0200

    docs: add comment for axios

--- menu-frontend/src/client/http-client.ts
+++ menu-frontend/src/client/http-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const httpClient = axios.create({
-  baseURL: 'https://menu-mingles-backend.azurewebsites.net/api/',
+  baseURL: 'https://menu-mingles-backend.azurewebsites.net/api/', // TODO: configure this somewhere?
   timeout: 10 * 1000,
   headers: {
     'Content-Type': 'application/json',
