966dc07f263bf7061170539129acda00325856de
Manuel <30572287+manuel-rw@users.noreply.github.com>
2025-08-23 10:25:01 +0200

    feat: add link to prodega

--- menu-frontend/src/views/account/Profile.vue
+++ menu-frontend/src/views/account/Profile.vue
@@ -5,10 +5,10 @@ import { useToast } from 'vue-toast-notification'
 const copyInviteLink = async () => {
   await navigator.clipboard.writeText(
     'https://menu-mingles-frontend-cccnfba0ezc2dhbc.northeurope-01.azurewebsites.net/auth/register?householdInviteCode=23892',
-  );
+  )
 
-  const $toast = useToast();
-  $toast.success('<i class="ti ti-circle-check-filled"></i> Invite link copied to clipboard');
+  const $toast = useToast()
+  $toast.success('<i class="ti ti-circle-check-filled"></i> Invite link copied to clipboard')
 }
 </script>
 
@@ -28,7 +28,7 @@ const copyInviteLink = async () => {
       </div>
     </div>
 
-    <div class="bg-neutral-200 rounded-2xl p-5">
+    <div class="bg-neutral-200 rounded-2xl p-5 mb-5">
       <h2 class="font-poetsen-one text-xl mb-3">Invite new people</h2>
 
       <div class="flex flex-row flex-nowrap gap-5">
@@ -56,6 +56,23 @@ const copyInviteLink = async () => {
         </div>
       </div>
     </div>
+
+    <a href="https://www.transgourmet.ch/de/pg" class="rounded-2xl">
+      <div class="bg-red-500 rounded-2xl p-5">
+        <div class="flex flex-nowrap items-center gap-5">
+          <i class="ti ti-external-link text-white text-5xl"></i>
+          <div>
+            <h2 class="font-poetsen-one text-white text-2xl">
+              Craving for fresh ingredients and foods?
+            </h2>
+            <p class="m-0 text-white">
+              Click here to check out our online store with unbeatable variety and over 25'000+
+              products
+            </p>
+          </div>
+        </div>
+      </div>
+    </a>
   </div>
 </template>
 
