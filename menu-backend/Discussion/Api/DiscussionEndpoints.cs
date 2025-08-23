e817805198d1e98816f26843755730011f3cbeb3
Marc Sallin <marc.sallin@outlook.com>
2025-08-23 23:46:54 +0200

    chore(backend): remove debug code

--- menu-backend/Discussion/Api/DiscussionEndpoints.cs
+++ menu-backend/Discussion/Api/DiscussionEndpoints.cs
@@ -37,13 +37,6 @@ public static class DiscussionEndpoints
             CancellationTokenSource cancellationTokenSource = new();
             cancellationTokenSource.CancelAfter(TimeSpan.FromMinutes(5));
 
-            // TODO: Remove, this is for debugging purposes only
-            //var output = JsonSerializer.Serialize(new DiscussionRequest(
-            //   household.People,
-            //    household.Chef!,
-            //    household.Consultants!,
-            //    selectedMenus));
-
             // Fire and forget that, to return to the UI
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             menuMinglersClient
@@ -57,7 +50,6 @@ public static class DiscussionEndpoints
             .ContinueWith(async response =>
             {
                 await menuResultStore.SaveMenuResult(session.HouseholdKey, session.SessionKey, response.Result!.Results);
-                return response.Result;
             });
 
             return Results.NoContent();
