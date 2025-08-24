22cd432764f5b4d53c67298b876e56562dc3d15f
Christoph Keller <christoph.keller@dotnetcorner.ch>
2025-08-24 02:56:12 +0200

    feat: added images to shoppinglist.

--- menu-frontend/src/components/ShoppingList.vue
+++ menu-frontend/src/components/ShoppingList.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import CustomImage from '@/components/CustomImage.vue'
+import { API_IMAGE_GEN_URL } from '@/constants'
+import { ref } from 'vue'
 
 const providers = [
   {
@@ -51,25 +52,28 @@ const selectedIndex = ref<(typeof providers)[number]['id']>('coop')
 function changeProvider(provider: (typeof providers)[number]) {
   selectedIndex.value = provider.id
 }
+
+function getImage(name: string) {
+  const url = new URL(API_IMAGE_GEN_URL)
+  url.searchParams.append('name', name)
+  return url.toString()
+}
 </script>
 
 <template>
   <div class="rounded-4xl bg-neutral-100 w-full flex flex-row gap-3 mt-5 mb-5">
     <button
-      @click="changeProvider(provider)"
-      v-for="(provider, index) in providers"
-      class="h-16 py-2.5 rounded-s-3xl w-full cursor-pointer"
-      :class="{ 'ps-3': index === 0, 'pe-3': index === providers.length - 1 }"
-    >
+            @click="changeProvider(provider)"
+            v-for="(provider, index) in providers"
+            class="h-16 py-2.5 rounded-s-3xl w-full cursor-pointer"
+            :class="{ 'ps-3': index === 0, 'pe-3': index === providers.length - 1 }">
       <div
-        class="h-full w-full rounded-3xl p-2"
-        :class="{ 'bg-neutral-300': selectedIndex === provider.id }"
-      >
+           class="h-full w-full rounded-3xl p-2"
+           :class="{ 'bg-neutral-300': selectedIndex === provider.id }">
         <img
-          :src="`/assets/food-store-logo/${provider.id}.png`"
-          :alt="`Logo ${provider.id}`"
-          class="h-full mx-auto object-contain"
-        />
+             :src="`/assets/food-store-logo/${provider.id}.png`"
+             :alt="`Logo ${provider.id}`"
+             class="h-full mx-auto object-contain" />
       </div>
     </button>
   </div>
@@ -78,7 +82,7 @@ function changeProvider(provider: (typeof providers)[number]) {
     <div class="flex flex-col gap-2">
       <div v-for="shoppingItem in items" class="flex flex-row items-center gap-5">
         <div class="w-24 h-14">
-          <CustomImage :src="shoppingItem.image" />
+          <CustomImage :src="getImage(shoppingItem.name)" />
         </div>
         <h5 class="text-neutral-800 font-bold">{{ shoppingItem.name }}</h5>
       </div>
